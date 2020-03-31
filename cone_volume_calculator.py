a42f49571493ba4431c983c7add265d91c9ee1d2
LeBugsBunny <thcytb@gmail.com>
2020-03-31 17:01:28 +0200

    This program will calculate the volume of a cone
    Signed-off-by: LeBugsBunny <thcytb@gmail.com>

--- cone_volume_calculator.py
+++ cone_volume_calculator.py
@@ -10,6 +10,6 @@ def vol_cone():
     area_base = math.pi * (radius * radius)
     third_area_base = area_base / 3
     volume = third_area_base * height
-    print(volume)
+    print(round(volume, 2))
 
 vol_cone()
